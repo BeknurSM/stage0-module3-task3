cf178f7f9160dc743e735ff20cff91df785af07d
BekoSm <b.smagulov.1999@gmail.com>
2022-07-11 12:18:05 +0600

    push#2

--- src/main/java/lang/print/gaps/task3/DeclaringVars.java
+++ src/main/java/lang/print/gaps/task3/DeclaringVars.java
@@ -4,7 +4,7 @@ public class DeclaringVars {
     public static void main(String[] args) {
         //write code here
         int a, b, c;
-        a = 10; b = 10; c = 10;
-        System.out.println(c);
+        a = b = c = 10;
+        System.out.println(c+"\n");
     }
 }
